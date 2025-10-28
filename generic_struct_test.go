461ed2a9b12baf27c688505c6200bb533205074e
herman-xphp <hermanxphp@gmail.com>
2025-10-28 11:33:30 +0800

    generic method

--- generic_struct_test.go
+++ generic_struct_test.go
@@ -3,6 +3,8 @@ package belajar_golang_generics
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type Data[T any] struct {
@@ -10,10 +12,28 @@ type Data[T any] struct {
 	Second T
 }
 
+func (d *Data[_]) SayHello(name string) string {
+	return "Hello " + name
+}
+
+func (d *Data[T]) ChangeFirst(first T) T {
+	d.First = first
+	return d.First
+}
+
 func TestData(t *testing.T) {
+	data := Data[string]{
+		First:  "Ucok",
+		Second: "Baba",
+	}
+	fmt.Println(data)
+}
+
+func TestGenericMethod(t *testing.T) {
 	data := Data[string]{
 		First: "Ucok",
 		Second: "Baba",
 	}
-	fmt.Println(data)
+
+	assert.Equal(t, "Budi", data.ChangeFirst("Budi"))
 }
