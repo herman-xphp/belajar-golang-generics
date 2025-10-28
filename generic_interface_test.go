cc803e1af58622bd32669a26aeb5f17c2a14d558
herman-xphp <hermanxphp@gmail.com>
2025-10-28 11:50:39 +0800

    generic interface

---
+++ generic_interface_test.go
@@ -0,0 +1,37 @@
+package belajar_golang_generics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type GetterSetter[T any] interface {
+	GetValue() T
+	SetValue(value T)
+}
+
+func ChangeValue[T any](param GetterSetter[T], value T) T {
+	param.SetValue(value)
+	return param.GetValue()
+}
+
+type MyData[T any] struct {
+	Value T
+}
+
+func (d *MyData[T]) GetValue() T {
+	return d.Value
+}
+
+func (d *MyData[T]) SetValue(value T) {
+	d.Value = value
+}
+
+func TestGenericInterface(t *testing.T) {
+	myData := MyData[string]{Value: "Budi"}
+	result := ChangeValue[string](&myData, "Ucok")
+
+	assert.Equal(t, "Ucok", result)
+	assert.Equal(t, "Ucok", myData.Value)
+}
